3c3ae07ba0dd6ba4904adcf0f29aad52c99d4659
Sainan Liu <umyta8@gmail.com>
2016-03-02 17:54:25 -0800

    Set up a simple scene to test out some basic photon setting.

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+    private const string VERSION = "v0.0.1";
+    private const int GROUP = 0;
+    public string playerPrefabName = "NetworkPlayer";
+    public string roomName = "Test";
+    public Transform spawnPopint;
+    public GameObject standbyCamera;
+
+    void Start()
+    {
+        // Enable photon network.
+        if (!PhotonNetwork.autoJoinLobby)
+        {
+            PhotonNetwork.autoJoinLobby = true;
+        }
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+	
+    }
+
+    void OnGUI()
+    {
+        // Prints current status
+        if (PhotonNetwork.autoJoinLobby)
+        {
+            // Debug.Log ("Connecting");
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else
+        {
+            // Debug.Log ("Offline");
+            GUILayout.Label("autoJoinLoby is disabled.");
+        }
+    }
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions()
+        { 
+            isVisible = false, maxPlayers = 4
+        };
+        PhotonNetwork.JoinOrCreateRoom(
+            roomName, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        standbyCamera.SetActive(false);
+        // Instantiate everything that every user should see here.
+        PhotonNetwork.Instantiate(
+            playerPrefabName,
+            spawnPopint.position,
+            spawnPopint.rotation, 
+            GROUP);
+    }
+}
---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+	public GameObject myCamera;
+
+	void Start ()
+	{
+		if (photonView.isMine) {
+			// Turn on everything that may cause conflicts during network playing.
+			myCamera.SetActive (true);	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
