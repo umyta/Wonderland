b1a7ec3bda543d53f5fb4483fd7e382db4e2ee9a
Lmonetti <sylviali1994@gmail.com>
2016-03-09 00:26:17 -0800

    Mouse click on player will bring up the menu and close it. Added door animation, but it only plays once

---
+++ Assets/Scripts/MenuAnimation/doorAnimation.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorAnimation : MonoBehaviour {
+    private bool isActive;
+    private bool setInitial;
+    private Quaternion originalRot;
+    private float count;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform innerDoor = transform.FindChild("door");
+        if (innerDoor != null && setInitial != true) {
+            originalRot = innerDoor.localRotation;
+            setInitial = true;
+        }
+
+        if (isActive)
+        {
+            openAnimation();
+        }
+        else {
+            reset();
+        }
+	}
+
+    public void openAnimation() {
+        Transform innerDoor = transform.FindChild("door");
+        if (innerDoor != null)
+        {
+            if (count > 45.0f)
+            {
+                return;
+            }
+            else {
+                innerDoor.Rotate(0f, 2f, 0f);
+                count += 1f;
+            }
+        }
+    }
+
+    public void setAnimationActive() {
+        isActive = true;
+    }
+
+    public void reset() {
+        Transform innerDoor = transform.FindChild("door");
+        innerDoor.localRotation = originalRot;
+        count = 0.0f;
+    }
+}
